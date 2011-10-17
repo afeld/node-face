8efc677703f76798ac60a4ab1f3eb9271f2d8d7e
Aidan Feldman <aidan.feldman@gmail.com>
2011-10-17 04:54:04 -0400

    created general-purpose prepareParams() method, which should be useful for any Face.com API method

--- face.js
+++ face.js
@@ -5,19 +5,10 @@ var _u = require('underscore'),
 var config = {};
 
 function doGetRequest(path, options){
-  options = _u.clone(options);
-  
   var success = options.success,
     error = options.error,
-    scope = options.scope || this;
-  
-  delete options.success;
-  delete options.error;
-  delete options.scope;
-  
-  _u.defaults(options, config);
-  var params = QueryString.stringify(options),
-    url = 'http://api.face.com' + path + '?' + params;
+    scope = options.scope || this,
+    url = 'http://api.face.com' + path + '?' + prepareParams(options);
   
   console.log('Face.com url:', url);
   Request({url: url, json: true}, function(err, response, data){
@@ -31,6 +22,30 @@ function doGetRequest(path, options){
   });
 }
 
+// returns a query string in the Face.com standard format:
+// nested params become comma-separated lists
+function prepareParams(options){
+  options = _u.clone(options);
+  
+  delete options.success;
+  delete options.error;
+  delete options.scope;
+  
+  for (var option in options){
+    var val = options[option];
+    if (_u.isArray(val)){
+      options[option] = val.join(',');
+    } else if (typeof val === 'object' && !_u.isFunction(val)){
+      options[option] = _u.reduce(val, function(memo, val, key){
+        return memo.concat(key + ':' + val);
+      }, []).join(',');
+    }
+  }
+  
+  _u.defaults(options, config);
+  return QueryString.stringify(options)
+}
+
 
 exports.init = function(key, secret){
   config.api_key = key;
@@ -42,16 +57,6 @@ exports.init = function(key, secret){
 exports.facebook = {
   get: function(options){
     options.namespace || (options.namespace = 'facebook.com');
-    
-    // convert nested params to strings
-    if (_u.isArray(options.uids)){
-      options.uids = options.uids.join(',');
-    }
-    var user_auth = options.user_auth;
-    if (typeof user_auth === 'object'){
-      options.user_auth = 'fb_user:' + user_auth.fb_user + ',fb_oauth_token:' + user_auth.fb_oauth_token;
-    }
-    
     doGetRequest('/facebook/get.json', options);
   }
 };
